2470c58457c3fe808a767e7e0817e30fc226fc4e
katsuhiro sato <katty@katty.local>
2022-05-19 11:45:21 +0900

    update: 1レコード取得用メソッド、一覧取得用メソッド、貸出日・返却日情報更新用メソッド、レコード削除用メソッドの追加

--- src/main/java/jp/co/seattle/library/service/RentalsService.java
+++ src/main/java/jp/co/seattle/library/service/RentalsService.java
@@ -1,8 +1,13 @@
 package jp.co.seattle.library.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+
+import jp.co.seattle.library.dto.RentalInfo;
+import jp.co.seattle.library.rowMapper.RentalInfoRowMapper;
 
 /**
  * 書籍サービス
@@ -14,42 +19,78 @@ public class RentalsService {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-
-	/**
-	 * 書籍をrentalsテーブルに追加する
-	 *
-	 * @param bookId 書籍ID
-	 */
-	public void rentalBook(int bookId) {
-		String sql = "INSERT INTO rentals (book_id) VALUES (?)";
-		jdbcTemplate.update(sql, bookId);
-	}
-
+	
 	/**
 	 * 書籍をrentalsテーブルの情報を取得する
 	 *
 	 * @param bookId 書籍ID
-	 * @return rentId 書籍ID
+	 * 
+	 * @return rentalInfo 貸出情報
 	 */
-	public int getRentInfo(int bookId) {
+	public RentalInfo getRentInfo(int bookId) {
 
-		String sql = "SELECT book_id FROM rentals where book_id = ?";
+		String sql = "SELECT book_id, title, rent_date, return_date FROM rentals where book_id = ?";
 		try {
-			int rentId = jdbcTemplate.queryForObject(sql, Integer.class, bookId);
-			return rentId;
+			RentalInfo rentalInfo = jdbcTemplate.queryForObject(sql, new RentalInfoRowMapper(), bookId);
+			return rentalInfo;
 		} catch (Exception e) {
-			return 0;
+			
+			return null;
 		}
 	}
 	
 	/**
-	 * 書籍をrentalsテーブルから削除する
+	 * rentalsテーブルの一覧を取得
+	 * 
+	 * @return rentList 貸出リスト
+	 */
+	public List<RentalInfo> getRentList(){
+		String sql = "SELECT book_id, title, rent_date, return_date FROM rentals ORDER BY title";
+		List<RentalInfo> rentList = jdbcTemplate.query(sql, new RentalInfoRowMapper());
+		
+		return rentList;
+	}
+	
+	/**
+	 * 書籍をrentalsテーブルに追加する
+	 *
+	 * @param bookId 書籍ID
+	 * @param title タイトル
+	 */
+	public void rentalBook(int bookId, String title) {
+		
+		String sql = "INSERT INTO rentals (book_id, rent_date, title) VALUES (?, now(), ?)";
+		jdbcTemplate.update(sql, bookId, title);
+	}
+	
+	/**
+	 * 書籍を貸出状態にする
+	 * 
+	 * @param bookId 書籍ID
+	 */
+	public void updateRentalBook(int bookId) {
+		String sql = "UPDATE rentals SET rent_date = now(), return_date = null WHERE book_id = ?";
+		jdbcTemplate.update(sql, bookId);
+	}
+	
+	/**
+	 * 書籍を返却状態にする
 	 *
 	 * @param bookId 書籍ID
 	 */
-	public void deleteBook(int bookId) {
-		
-		String sql = "DELETE FROM rentals WHERE book_id = ?";
+	public void updateReturnBook(int bookId) {
+	
+		String sql = "UPDATE rentals SET rent_date = null, return_date = now() WHERE book_id = ?";
 		jdbcTemplate.update(sql, bookId);
+	}
+	
+	/**
+	 * rentalsテーブルの書籍を削除する
+	 * 
+	 * @param bookId 書籍ID
+	 */
+	public void deleteRent(int bookId) {
+		String sql = "DELETE FROM rentals WHERE book_id = ?;";
+    	jdbcTemplate.update(sql, bookId);
 	}
 }
